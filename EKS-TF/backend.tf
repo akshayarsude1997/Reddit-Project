8b9f39497ae001546ccf9745d91f1962be5f973d
akshayarsude1997 <114069175+akshayarsude1997@users.noreply.github.com>
2025-02-12 10:43:56 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "new-on1"
+    bucket         = "my-new-bucket-97"
     region         = "us-east-1"
     key            = "EKS-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "new-tf-db-table"
+    dynamodb_table = "Lock-Files"
     encrypt        = true
   }
   required_version = ">=0.13.0"
